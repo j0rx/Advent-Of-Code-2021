d64159eb4b24c5e7bab120e033ee3d8c6479c32c
j0rx <jolle.ans@hotmail.com>
2021-12-06 14:13:28 +0100

    Advent Of Code 2021 Day 6

---
+++ Advent of Code 6/Bonus.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_6
+{
+    class Bonus
+    {
+        int CountFishRecursive(int days, byte[] inputs, int toDays)
+        {
+            if (days == toDays)
+                return inputs.Length;
+            var birthCount = 0;
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                if (inputs[i] == 0)
+                {
+                    birthCount += 1;
+                    inputs[i] = 6;
+                }
+                else
+                    inputs[i] -= 1;
+            }
+            byte[] newInputs = new byte[birthCount];
+            for (int i = 0; i < birthCount; i++)
+            {
+                newInputs[i] = 8;
+            }
+            return CountFishRecursive(days + 1, inputs.Concat(newInputs).ToArray(), toDays);
+        }
+        int CountFishObject(int days, byte[] inputs)
+        {
+            var fishList = new List<LanternFish>();
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                fishList.Add(new LanternFish(inputs[i]));
+            }
+            for (int i = 0; i < days; i++)
+            {
+                var currentFishCount = fishList.Count;
+                for (int j = 0; j < currentFishCount; j++)
+                {
+                    if (fishList[j].GiveBirth())
+                    {
+                        fishList.Add(new LanternFish(8));
+                    }
+                }
+            }
+            return fishList.Count;
+        }
+    }
+    class LanternFish
+    {
+        public int DaysUntilBirth { get; set; }
+        public LanternFish(int daysUntilBirth)
+        {
+            DaysUntilBirth = daysUntilBirth;
+        }
+        public bool GiveBirth()
+        {
+            if (DaysUntilBirth == 0)
+            {
+                DaysUntilBirth = 6;
+                return true;
+            }
+            else
+                DaysUntilBirth -= 1;
+            return false;
+        }
+    }
+}
---
+++ Advent of Code 6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Advent_of_Code_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllText("C:/Users/jolle/Desktop/advent of code/Advent Of Code 2021/Advent Of Code 6/input.txt");
+            byte[] inputs = lines.Split(',').Select(l=>byte.Parse (l)).ToArray();
+            var fishCountPart2 = CountFish(256, inputs, 6 , 8);
+            var fishCountpart1 = CountFish(80, inputs, 6, 8);
+            Console.WriteLine($"Advent 6!\nPart 1: {fishCountpart1}\nPart 2: {fishCountPart2}");
+            Console.ReadKey();
+
+            long CountFish(int days, byte[] inputs, int birthFrequency, int maxAge)
+            {
+                long[] inputsSum = new long[maxAge+2];
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    inputsSum[inputs[i]] += 1;
+                }
+                for (int i = 0; i < days; i++)
+                {
+
+                    inputsSum[inputsSum.Length-1] = inputsSum[0];
+                    inputsSum[birthFrequency+1] += inputsSum[0];
+                    for (int j = 1; j < inputsSum.Length; j++)
+                    {
+                        inputsSum[j - 1] = inputsSum[j];
+                    }
+                    inputsSum[inputsSum.Length - 1] = 0;
+
+                }
+                return inputsSum.Sum();
+            }
+        }
+    }
+}
